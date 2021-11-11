65871bf02216c82d07d3d83620c40e05fb436158
AndreeJait <panjaitanandree@gmail.com>
2021-11-11 10:09:54 +0700

    Update ENV

--- manta_app/settings.py
+++ manta_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=^sud$(ncsnv71#&khwnot#745im*xke@$ju&f+5$$x@-b#@u1'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
